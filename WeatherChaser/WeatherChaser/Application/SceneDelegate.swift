47dde0b40d4c5a164e1b72924cd5341919240219
SONG <geun2121@gmail.com>
2023-12-03 17:23:10 +0900

    [ADD] 프로젝트 생성 및 불필요한 것들 제거
    - 프로젝트를 생성하였다.
    - Main.storyBoard 제거
    - ViewController.swift 제거
    - 불필요한 주석 제거

---
+++ WeatherChaser/WeatherChaser/Application/SceneDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  SceneDelegate.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/03.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+//        guard let scene = (scene as? UIWindowScene) else { return }
+//        
+//        self.window = UIWindow(windowScene: scene)
+//        
+//        guard let window = self.window else { return }
+//        
+//        let coordinator = RootViewControllerCoordinator(window)
+//        coordinator.changeViewController()
+        
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+
+    }
+
+
+}
+
