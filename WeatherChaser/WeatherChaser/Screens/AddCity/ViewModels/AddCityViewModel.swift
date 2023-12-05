e35dfa7c3a1bd0fb0fe68c30a37fd6f20aa23f23
SONG <geun2121@gmail.com>
2023-12-05 19:39:54 +0900

    [FEAT] addWeatherCellViewModel() 구현
    - api연동을 위한 fetch() 메소드 호출부 클로저 구현

--- WeatherChaser/WeatherChaser/Screens/AddCity/ViewModels/AddCityViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/AddCity/ViewModels/AddCityViewModel.swift
@@ -8,5 +8,23 @@
 import Foundation
 
 class AddCityViewModel {
-    
+    func addWeatherCellViewModel(for city: String, completion: @escaping (WeatherCellViewModel) -> Void) {
+        let weatherURL = URLManager.setURLforWeather(of: city)
+        
+        let weatherResource = Resource<WeatherResponse>(url: weatherURL) { data in
+            let weatherResponse = try? JSONDecoder().decode(WeatherResponse.self, from: data)
+            return weatherResponse
+        }
+        
+        NetworkManager().fetch(resource: weatherResource) { (result) in
+            switch result {
+            case .success(let weatherResponse):
+                    let viewModel = WeatherCellViewModel(weather: weatherResponse)
+                    completion(viewModel)
+
+            case .failure(let error):
+                print("Network request error: \(error)")
+            }
+        }
+    }
 }
