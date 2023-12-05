0c0e6ac0b1a8a16955b93a2eeae30a0f8fcdbf63
SONG <geun2121@gmail.com>
2023-12-05 19:40:53 +0900

    [FEAT] test()
    - VC에 test()메소드 구현
    - 응답이 잘 도착했는지 print로 확인한다.

--- WeatherChaser/WeatherChaser/Screens/AddCity/Controllers/AddCityViewController.swift
+++ WeatherChaser/WeatherChaser/Screens/AddCity/Controllers/AddCityViewController.swift
@@ -8,8 +8,17 @@
 import UIKit
 
 class AddCityViewController : UIViewController {
+    private var addCityViewModel = AddCityViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .green
+        test()
+    }
+    
+    private func test() {
+        addCityViewModel.addWeatherCellViewModel(for: "Seoul") { result in
+            print(result)
+        }
     }
 }
