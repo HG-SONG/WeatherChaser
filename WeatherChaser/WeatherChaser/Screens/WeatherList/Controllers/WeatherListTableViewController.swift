4c75b9fd15458aa0f692b233151e05bdbb2c1dc8
SONG <geun2121@gmail.com>
2023-12-05 16:31:56 +0900

    [FEAT] WeatherListTableVC BarButton
    - BarButton 생성 및 동작 정의

--- WeatherChaser/WeatherChaser/Screens/WeatherList/Controllers/WeatherListTableViewController.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherList/Controllers/WeatherListTableViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class WeatherListTableViewController: UITableViewController {
     
+    private var addCityButton : UIBarButtonItem!
+    private var settingsButton : UIBarButtonItem!
     var delegate: ButtonActionDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        commonInit()
+    }
+    
+    private func commonInit() {
+        setBarButtons()
+    }
+    
+    private func setBarButtons() {
+        self.settingsButton = UIBarButtonItem(title: "Settings", style: .plain, target:self, action: #selector(settingsButtonTouched))
+        
+        self.addCityButton = UIBarButtonItem(title: "Add City", style: .plain, target:self, action: #selector(addCityButtonTouched))
+        
+        self.navigationItem.setLeftBarButton(settingsButton, animated: true)
+        
+        self.navigationItem.setRightBarButton(addCityButton, animated: true)
+    }
+    
     @objc private func addCityButtonTouched() {
         delegate?.addCityButtonTouched?()
     }
