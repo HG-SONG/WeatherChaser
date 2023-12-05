d9e3a8fadecad1782f5321c49a6f6a0bb4808192
SONG <geun2121@gmail.com>
2023-12-05 18:48:23 +0900

    [FEAT] ViewModel 구현
    - WeatherList에서 사용될 ViewModel을 구현하였다.

--- WeatherChaser/WeatherChaser/Screens/WeatherList/ViewModels/WeatherCellViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherList/ViewModels/WeatherCellViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class WeatherCellViewModel {
+    let weather : WeatherResponse
+    var temperature: Double
+    var feelsLikeTemperature: Double
+    
+    init(weather: WeatherResponse) {
+        self.weather = weather
+        self.temperature = weather.main.temp
+        self.feelsLikeTemperature = weather.main.feels_like
+    }
+    
+    var city: String {
+        return weather.name
+    }
+    
+}
--- WeatherChaser/WeatherChaser/Screens/WeatherList/ViewModels/WeatherListViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherList/ViewModels/WeatherListViewModel.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class WeatherListViewModel {
+    private var weatherCellViewModels : [WeatherCellViewModel] = []
     
+    init(weathers: [WeatherCellViewModel]) {
+        self.weatherCellViewModels = weathers
+    }
+    
+    func numberOfRows(_ section: Int?) -> Int {
+        return weatherCellViewModels.count
+    }
+    
+    func modelAt(_ index: Int) -> WeatherCellViewModel {
+        return weatherCellViewModels[index]
+    }
 }
