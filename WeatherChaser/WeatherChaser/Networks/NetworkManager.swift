baaa606dc2cdd07b2490faecfa67c301ee6938a2
SONG <geun2121@gmail.com>
2023-12-05 18:16:31 +0900

    [FEAT] fetch() 메소드 구현
    - 요청 메소드 구현

--- WeatherChaser/WeatherChaser/Networks/NetworkManager.swift
+++ WeatherChaser/WeatherChaser/Networks/NetworkManager.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    func fetch<T>(resource: Resource<T>, completion: @escaping (T?) -> ()) {
+        
+        URLSession.shared.dataTask(with: resource.url) { data, response, error in
+            if let data = data {
+                DispatchQueue.main.async {
+                     completion(resource.parsed(data))
+                }
+            } else {
+                completion(nil)
+            }
+        }.resume()
+    }
+}
+
+struct Resource<T> {
+    let url: URL
+    let parsed: (Data) -> T?
+}
