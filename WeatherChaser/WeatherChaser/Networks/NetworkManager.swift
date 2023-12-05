c06823ae35b2ea5297066754c00e5cdb09377e16
SONG <geun2121@gmail.com>
2023-12-05 17:38:29 +0900

    [ADD] 파일추가
    - api연동에 필요한 파일들을 추가하였다.

---
+++ WeatherChaser/WeatherChaser/Networks/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/05.
+//
+
+import Foundation
---
+++ WeatherChaser/WeatherChaser/Screens/WeatherList/ViewModels/WeatherListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherListViewModel.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/04.
+//
+
+import Foundation
+
+class WeatherListViewModel {
+    
+}
