fe4b5fb0791be674d6c6da7a543b50c34a812299
SONG <geun2121@gmail.com>
2023-12-10 17:55:25 +0900

    [ADD] 파일 추가
    - DoubleExtension , WeatherCell 파일을 추가했다.

---
+++ WeatherChaser/WeatherChaser/Extensions/DoubleExtension.swift
@@ -0,0 +1,8 @@
+//
+//  DoubleExtension.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/08.
+//
+
+import Foundation
---
+++ WeatherChaser/WeatherChaser/Screens/WeatherList/Views/WeatherCell.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherCell.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/08.
+//
+
+import Foundation
