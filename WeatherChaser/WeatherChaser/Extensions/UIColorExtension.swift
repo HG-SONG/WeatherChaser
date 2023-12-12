0fe7d00fb6fbe9fa4f5c0606c1111446c996a03d
SONG <geun2121@gmail.com>
2023-12-12 19:59:52 +0900

    [FEAT] UIColor Extension
    - 커스텀컬러를 정의해주었다.

--- WeatherChaser/WeatherChaser/Extensions/UIColorExtension.swift
+++ WeatherChaser/WeatherChaser/Extensions/UIColorExtension.swift
@@ -5,4 +5,10 @@
 //  Created by SONG on 2023/12/12.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let maxTemp = UIColor(red: 255/255, green: 150/255, blue: 138/255, alpha: 1)
+    static let feelsTemp = UIColor(red: 204/255, green: 226/255, blue: 203/255, alpha: 1)
+    static let minTemp = UIColor(red: 162/255, green: 225/255, blue: 219/255, alpha: 1)
+}
