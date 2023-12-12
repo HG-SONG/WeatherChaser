4db1d33249cb0025ce9bde81fe1cfb47817dda96
SONG <geun2121@gmail.com>
2023-12-12 14:39:37 +0900

    [FEAT] WeatherCellViewModel 프로퍼티 추가
    - 모델변경사항 적용
    - iconDest , time , backgroundImageName 구현

--- WeatherChaser/WeatherChaser/Screens/WeatherList/ViewModels/WeatherCellViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherList/ViewModels/WeatherCellViewModel.swift
@@ -22,4 +22,58 @@ class WeatherCellViewModel {
         return weather.name
     }
     
+    var iconDesc: String {
+        guard let iconDesc = self.weather.weather.first?.main else {
+            return IconType.nothing.rawValue
+        }
+        
+        switch iconDesc {
+        case "Snow":
+            return IconType.snow.rawValue
+        case "Rain":
+            return IconType.rain.rawValue
+        case "Clouds":
+            return IconType.clouds.rawValue
+        default :
+            return IconType.nothing.rawValue
+        }
+    }
+    
+    var time: String {
+        let currentTime = self.weather.dt
+        let sunriseTime = self.weather.sys.sunrise
+        let sunsetTime = self.weather.sys.sunset
+        
+        let timeGap = 1800 
+        
+        if currentTime >= sunriseTime - timeGap && currentTime <= sunriseTime + timeGap {
+            return "sunrise"
+        } else if currentTime >= sunsetTime - timeGap && currentTime <= sunsetTime + timeGap  {
+            return "sunset"
+        } else if currentTime > sunriseTime + timeGap && currentTime < sunsetTime - timeGap {
+            return "day"
+        } else {
+            return "night"
+        }
+    }
+    
+    var backgroundImageName : String {
+        var wd :String
+        
+        switch self.iconDesc {
+        case IconType.snow.rawValue :
+            wd = "snow"
+            return wd + "_" + time + ".jpg"
+        case IconType.rain.rawValue :
+            wd = "rain"
+            return wd + "_" + time + ".jpg"
+        case IconType.clouds.rawValue :
+            wd = "clouds"
+            return wd + "_" + time + ".jpg"
+        default:
+            wd = "nothing"
+            return wd + "_" + time + ".jpg"
+        }
+    }
+    
 }
