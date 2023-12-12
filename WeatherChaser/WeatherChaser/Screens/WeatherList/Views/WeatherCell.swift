b770ed44f8c4c9d6d05cee6bf22a57b372becfef
SONG <geun2121@gmail.com>
2023-12-12 14:40:41 +0900

    [FEAT] backgroundImageView 추가
    - ImageView 생성
    - 기본 세팅
    - layout

--- WeatherChaser/WeatherChaser/Screens/WeatherList/Views/WeatherCell.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherList/Views/WeatherCell.swift
@@ -11,19 +11,39 @@ class WeatherCell: UITableViewCell {
     static let identifier = "WeatherCell"
     private var nameLabel = UILabel(frame: .zero)
     private var temperatureLabel = UILabel(frame: .zero)
+    private var backgoundImageView = UIImageView(frame: .zero)
     
     func commonInit(_ viewModel: WeatherCellViewModel) {
+        self.backgoundImageView.image = UIImage(named: viewModel.backgroundImageName)
+        self.backgoundImageView.contentMode = .scaleAspectFill
+        
         self.nameLabel.text = viewModel.city
         self.nameLabel.font = self.nameLabel.font.withSize(40.0)
         
         self.temperatureLabel.text = viewModel.temperature.formatAsDegree()
         self.temperatureLabel.font = self.temperatureLabel.font.withSize(40.0)
+        
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        layoutBackbroundImageView()
         layoutNameLabel()
         layoutTemperatureLabel()
+        layoutWeatherSymbol()
+    }
+    
+    private func layoutBackbroundImageView() {
+        if !self.subviews.contains(backgoundImageView) {
+            self.addSubview(backgoundImageView)
+        }
+        
+        backgoundImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                
+            ]
+        )
     }
     
     private func layoutNameLabel() {
