797ac431e4023d974dabfa6fa30449256d1448c4
SONG <geun2121@gmail.com>
2023-12-12 14:36:11 +0900

    [FEAT] IconType 정의
    - 날씨별 아이콘조건 분기

---
+++ WeatherChaser/WeatherChaser/Screens/WeatherList/ViewModels/IconType.swift
@@ -0,0 +1,15 @@
+//
+//  iconType.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/11.
+//
+
+import Foundation
+
+enum IconType : String {
+    case snow = "snow"
+    case rain = "cloud.rain.fill"
+    case clouds = "cloud.fill"
+    case nothing = "sun.max.fill"
+}
