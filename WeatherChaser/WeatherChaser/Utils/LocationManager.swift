a00d1fc6838ea34bb9ae38c9df4b0b5f8ad464b3
SONG <geun2121@gmail.com>
2023-12-14 18:40:38 +0900

    [FEAT] LocationManager 구현
    - init , delegate 메소드 구현

--- WeatherChaser/WeatherChaser/Utils/LocationManager.swift
+++ WeatherChaser/WeatherChaser/Utils/LocationManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    
+    private var locationManager = CLLocationManager()
+    private var completionHandler: ((CLLocation) -> Void)?
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            completionHandler?(location)
+            completionHandler = nil
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location request 실패: \(error.localizedDescription)")
+        completionHandler = nil
+    }
+}
