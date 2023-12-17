e5cdd5bf47ec07991ab244ef12c13c70a9592a74
SONG <geun2121@gmail.com>
2023-12-17 16:02:18 +0900

    [FEAT] textField 배치
    - textField 프로퍼티 설정
    - 레이아웃 설정
    - 배경색 설정

--- WeatherChaser/WeatherChaser/Screens/AddCity/Controllers/AddCityViewController.swift
+++ WeatherChaser/WeatherChaser/Screens/AddCity/Controllers/AddCityViewController.swift
@@ -10,19 +10,39 @@ import UIKit
 class AddCityViewController : UIViewController {
     private var addCityViewModel = AddCityViewModel()
     private var saveButton : UIBarButtonItem!
+    private let cityNameTextField = UITextField(frame: .zero)
     
     var delegate: AddCityDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .green
+        self.view.backgroundColor = .black
         setBarButtons()
+        setCityNameTextField()
     }
     
     private func setBarButtons() {
         self.saveButton = UIBarButtonItem(title: "Save", style: .plain, target:self, action: #selector(saveButtonTouched))
         
         self.navigationItem.setRightBarButton(saveButton, animated: true)
+    }
+    
+    private func setCityNameTextField() {
+        self.cityNameTextField.placeholder = "Enter the city name"
+        self.cityNameTextField.borderStyle = .roundedRect
+        self.cityNameTextField.textColor = .white
+        self.cityNameTextField.backgroundColor = .darkGray
+        
+        self.view.addSubview(cityNameTextField)
+        
+        self.cityNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                cityNameTextField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+                cityNameTextField.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 50.0)
+            ]
+        )
     }
     
     @objc private func saveButtonTouched() {
