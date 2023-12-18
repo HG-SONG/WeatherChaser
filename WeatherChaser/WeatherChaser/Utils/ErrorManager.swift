1e9e34dbff155a643709f21eaa3d1c31e3e2e0bf
SONG <geun2121@gmail.com>
2023-12-18 18:07:51 +0900

    [FIX] 메소드 수정
    - UI업데이트와 관련된 메소드를 메인스레드에서 실행하도록 변경하였다.

--- WeatherChaser/WeatherChaser/Utils/ErrorManager.swift
+++ WeatherChaser/WeatherChaser/Utils/ErrorManager.swift
@@ -10,22 +10,22 @@ import UIKit
 class ErrorManager {
     
     static func showAlert(error: NetworkError) {
-        var message : String
-        
-        switch error {
-        case .decodingFailure :
-            message = "The city could not be found. Check the city name"
-        case .noData :
-            message = "No response."
-        case .offline :
-            message = "Can not connect to server. Check your network connection"
-        }
-        
-        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(okAction)
-        
         DispatchQueue.main.async {
+            var message : String
+            
+            switch error {
+            case .decodingFailure :
+                message = "The city could not be found. Check the city name"
+            case .noData :
+                message = "No response."
+            case .offline :
+                message = "Can not connect to server. Check your network connection"
+            }
+            
+            let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            
             guard let topViewController = UIApplication.shared.windows.first?.rootViewController else {
                 return
             }
@@ -34,11 +34,12 @@ class ErrorManager {
     }
     
     static func showAlertForUnknown() {
-        let alertController = UIAlertController(title: "Error", message: "Unknown Error", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alertController.addAction(okAction)
-        
         DispatchQueue.main.async {
+            let alertController = UIAlertController(title: "Error", message: "Unknown Error", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertController.addAction(okAction)
+            
+            
             if let topViewController = UIApplication.shared.windows.first?.rootViewController {
                 topViewController.present(alertController, animated: true, completion: nil)
             }
