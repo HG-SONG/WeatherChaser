86c45dfecdbd823f78117b52f081f91a152eda30
SONG <geun2121@gmail.com>
2023-12-18 17:10:44 +0900

    [FEAT] 메소드 추가
    - Alert를 보여주는 메소드를 구현하였다.
    - static 으로 선언하여 바로 호출 할 수 있게 했다.

--- WeatherChaser/WeatherChaser/Utils/ErrorManager.swift
+++ WeatherChaser/WeatherChaser/Utils/ErrorManager.swift
@@ -5,4 +5,43 @@
 //  Created by SONG on 2023/12/17.
 //
 
-import Foundation
+import UIKit
+
+class ErrorManager {
+    
+    static func showAlert(error: NetworkError) {
+        var message : String
+        
+        switch error {
+        case .decodingFailure :
+            message = "The city could not be found. Check the city name"
+        case .noData :
+            message = "No response."
+        case .offline :
+            message = "Can not connect to server. Check your network connection"
+        }
+        
+        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        
+        DispatchQueue.main.async {
+            guard let topViewController = UIApplication.shared.windows.first?.rootViewController else {
+                return
+            }
+            topViewController.present(alertController, animated: true)
+        }
+    }
+    
+    static func showAlertForUnknown() {
+        let alertController = UIAlertController(title: "Error", message: "Unknown Error", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        
+        DispatchQueue.main.async {
+            if let topViewController = UIApplication.shared.windows.first?.rootViewController {
+                topViewController.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+}
