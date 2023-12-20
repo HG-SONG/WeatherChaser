8fbe331f3b2505b4d9b3bd92d862eec4cbb46103
SONG <geun2121@gmail.com>
2023-12-20 17:45:15 +0900

    [FEAT] tableViewDelegate 구현
    - delegate , dataSrc 메소드 구현
    - cell register

--- WeatherChaser/WeatherChaser/Screens/Settings/Controllers/SettingsTableViewController.swift
+++ WeatherChaser/WeatherChaser/Screens/Settings/Controllers/SettingsTableViewController.swift
@@ -18,6 +18,7 @@ class SettingsTableViewController : UITableViewController {
         super.viewDidLoad()
         setBarButtons()
         self.view.backgroundColor = .black
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "SettingsCell")
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -34,5 +35,54 @@ class SettingsTableViewController : UITableViewController {
     @objc private func saveButtonTouched() {
         self.settingsViewModel.selectedUnit = self.selectedUnit!
         delegate?.saveButtonTouched()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsViewModel.units.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let settingsItem = settingsViewModel.units[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
+        cell.textLabel?.text = settingsItem.displayName
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 25.0, weight: .bold)
+        
+        if settingsItem == settingsViewModel.selectedUnit {
+            cell.accessoryType = .checkmark
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let settingsItem = settingsViewModel.units[indexPath.row]
+        
+        if settingsItem == settingsViewModel.selectedUnit {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.visibleCells.forEach { cell in
+            cell.accessoryType = .none
+        }
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+            let unit = Unit.allCases[indexPath.row]
+            self.selectedUnit = unit
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
     }
 }
