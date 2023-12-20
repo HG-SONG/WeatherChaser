b1ad07cc0300752b46423212749180bf7eee419c
SONG <geun2121@gmail.com>
2023-12-20 17:18:23 +0900

    [FEAT] ViewModel 정의
    - settingsViewModel을 정의했다.
    - UserDefault를 사용하여 unit을 세팅한다.

--- WeatherChaser/WeatherChaser/Screens/Settings/ViewModels/SettingsViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/Settings/ViewModels/SettingsViewModel.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class SettingsViewModel {
-
+    let units = Unit.allCases
+    
+    var selectedUnit: Unit {
+        get{
+            let userDefaults = UserDefaults.standard
+            var unitValue = ""
+            if let value = userDefaults.value(forKey: "unit") as? String {
+                unitValue = value
+            }
+            return Unit(rawValue: unitValue)!
+        }
+        
+        set{
+            let userDefault = UserDefaults.standard
+            userDefault.set(newValue.rawValue, forKey:"unit")
+            userDefault.set(newValue.rawValue, forKey:"selected")
+        }
+    }
 }
