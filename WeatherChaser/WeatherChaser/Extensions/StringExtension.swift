9ecc0021008cc645e0151cee0fefdee0ff1e6b92
SONG <geun2121@gmail.com>
2023-12-20 18:04:18 +0900

    [FEAT] Unit변경 메소드 구현
    - Cel <-> Fah 전환 메소드 구현
    - WeatherListVC appear시 동작
    - 단위별 기호 세팅

--- WeatherChaser/WeatherChaser/Extensions/StringExtension.swift
+++ WeatherChaser/WeatherChaser/Extensions/StringExtension.swift
@@ -11,4 +11,18 @@ extension String {
     func replaceSpacesWithDash() -> String {
         return self.replacingOccurrences(of: " ", with: "-")
     }
+    
+    func makeUnitSymbol() -> String {
+        let unit : Unit.RawValue = UserDefaults.standard.value(forKey: "selected") as! Unit.RawValue
+        var symbol : String = ""
+        switch unit {
+        case Unit.celsius.rawValue :
+            symbol = "℃"
+        case Unit.fahrenheit.rawValue :
+            symbol = "℉"
+        default:
+            symbol = "?"
+        }
+        return self + symbol
+    }
 }
