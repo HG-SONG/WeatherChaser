4c050f17606005fd92fbfec03935cddd0b55a9ae
SONG <geun2121@gmail.com>
2023-12-20 17:44:24 +0900

    [FEAT] SettingsTableViewController 구현
    - 프로퍼티 및 버튼delegate 동작 구현

--- WeatherChaser/WeatherChaser/Screens/Settings/Controllers/SettingsTableViewController.swift
+++ WeatherChaser/WeatherChaser/Screens/Settings/Controllers/SettingsTableViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class SettingsTableViewController : UITableViewController {
     
+    private var settingsViewModel = SettingsViewModel()
+    private var selectedUnit : Unit?
+    private var saveButton : UIBarButtonItem!
     var delegate : SaveDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        setBarButtons()
+        self.view.backgroundColor = .black
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        selectedUnit = settingsViewModel.selectedUnit
+    }
+    
+    private func setBarButtons() {
+        self.saveButton = UIBarButtonItem(title: "Save", style: .plain, target:self, action: #selector(saveButtonTouched))
+        
+        self.navigationItem.setRightBarButton(saveButton, animated: true)
+    }
+    
+    @objc private func saveButtonTouched() {
+        self.settingsViewModel.selectedUnit = self.selectedUnit!
+        delegate?.saveButtonTouched()
     }
 }
