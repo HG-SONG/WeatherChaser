93fd79b67e0dc078f21c29ba0f3676788143cc88
SONG <geun2121@gmail.com>
2023-12-20 17:47:44 +0900

    [FEAT] userDefault 추가
    - unit관련 K-V 추가
    - seleceted unit을 userDefault로 관리한다.

--- WeatherChaser/WeatherChaser/Application/AppDelegate.swift
+++ WeatherChaser/WeatherChaser/Application/AppDelegate.swift
@@ -42,6 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let userDefaults = UserDefaults.standard
         if userDefaults.value(forKey: "unit") == nil {
             userDefaults.set("metric", forKey: "unit")
+            userDefaults.set("metric", forKey: "selected")
         }
     }
 }
