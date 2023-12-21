d3921b2b09f6bf32aef5049e31670c4a5abba96a
SONG <geun2121@gmail.com>
2023-12-22 01:16:18 +0900

    [ADD] 파일추가
    - indexPath를 표시하는 test용 cell을 추가하였다.

---
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/Views/TestCollectionViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  TestCollectionViewCell.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/21.
+//
+
+import UIKit
+
+class TestCollectionViewCell: UICollectionViewCell {
+    static let identifier = "testCell"
+    private var titleLabel = UILabel(frame: .zero)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.contentView.backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.contentView.backgroundColor = .red
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutTitleLabel()
+    }
+    
+    func setTitleLabel(text:String) {
+        self.titleLabel.text = text
+    }
+    
+    private func layoutTitleLabel() {
+        if !self.contentView.subviews.contains(titleLabel) {
+            self.contentView.addSubview(titleLabel)
+        }
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                titleLabel.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
+                titleLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor)
+            ]
+        )
+    }
+}
