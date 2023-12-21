8b46b6c3ede9cd31d8d8ffa7bc46439877c8971c
SONG <geun2121@gmail.com>
2023-12-21 16:39:31 +0900

    [ADD] 파일추가 및 파일이름변경
    - 코디네이터와 뷰컨을 추가했다.
    - 프로토콜 이름을 변경하였다.

---
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/Controllers/WeatherDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherDetailViewController.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/21.
+//
+
+import UIKit
+
+class WeatherDetailViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .darkGray
+    }
+}
