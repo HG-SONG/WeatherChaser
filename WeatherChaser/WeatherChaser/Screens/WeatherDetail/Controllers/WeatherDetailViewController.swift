4afd44e23b888dfb6f70c84070a10595e448d6bd
SONG <geun2121@gmail.com>
2023-12-22 01:25:26 +0900

    [FEAT] section정의 및 필수메소드 구현
    - 총 4개의 섹션에 서로 다른 아이템 개수를 정의하였다.
    - 각 섹션의 1row당 들어갈 col에 따라 분기하여 처리하였다.
    - section0 : 2col
    - section1 : 1col
    - section2 : 2col * 2row
    - section3 : 1col

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/Controllers/WeatherDetailViewController.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/Controllers/WeatherDetailViewController.swift
@@ -19,6 +19,7 @@ class WeatherDetailViewController : UIViewController {
     private func makeCollectionView() {
         let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
             print(sectionIndex)
+            return self.createSection(for: sectionIndex)
         }
 
         self.collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
@@ -29,4 +30,82 @@ class WeatherDetailViewController : UIViewController {
         view.addSubview(collectionView!)
     }
 }
+
+extension WeatherDetailViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+                case 0:
+                    return 2
+                case 1:
+                    return 1
+                case 2:
+                    return 4
+                case 3:
+                    return 1
+                default:
+                    return 0
+                }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TestCollectionViewCell.identifier, for: indexPath) as? TestCollectionViewCell else {
+            return UICollectionViewCell(frame: .zero)
+        }
+        cell.setTitleLabel(text: indexPath.description)
+        return cell
+    }
+    
+    private func createSection(for sectionIndex: Int) -> NSCollectionLayoutSection {
+        switch sectionIndex {
+        case 0:
+            return createTwoColumnSection(weightConstant: sectionIndex)
+        case 1:
+            return createSingleColumnSection()
+        case 2:
+            return createTwoColumnSection(weightConstant: sectionIndex)
+        case 3:
+            return createHorizontalScrollingSection()
+        default:
+            fatalError("Invalid section index")
+        }
+    }
+    
+    private func createTwoColumnSection(weightConstant :Int) -> NSCollectionLayoutSection {
+        let weight = (Double(weightConstant) * 0.05)
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(0.98))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.1 + weight))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    private func createSingleColumnSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.98))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.05))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    private func createHorizontalScrollingSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.4))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        return section
+    }
 }
