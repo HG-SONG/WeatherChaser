d5f14e5fc7b28d1e860736b3127817dd528f496d
SONG <geun2121@gmail.com>
2023-12-22 01:19:56 +0900

    [FEAT] 콜렉션뷰 생성
    - delegate ,dataSrc 지정
    - testCell 등록
    - compositionalLayout 채택

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/Controllers/WeatherDetailViewController.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/Controllers/WeatherDetailViewController.swift
@@ -8,8 +8,25 @@
 import UIKit
 
 class WeatherDetailViewController : UIViewController {
+    private var collectionView : UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .darkGray
+        makeCollectionView()
     }
+    
+    private func makeCollectionView() {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            print(sectionIndex)
+        }
+
+        self.collectionView = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
+        self.collectionView!.delegate = self
+        self.collectionView!.dataSource = self
+        self.collectionView!.register(TestCollectionViewCell.self, forCellWithReuseIdentifier: TestCollectionViewCell.identifier)
+        self.collectionView!.backgroundColor = .black
+        view.addSubview(collectionView!)
+    }
+}
 }
