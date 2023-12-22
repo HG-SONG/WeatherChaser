c0cd887f7c83262d2d69507a4000d7c82871d5ee
SONG <geun2121@gmail.com>
2023-12-23 04:53:30 +0900

    [FEAT] WeatherDetailViewModel 정의
    - 섹션별로 cell이 차이가 있기 때문에 viewModel을 내부적으로 한번 더 분리 시켰다.
    - 이미 받아왔던 데이터의 경우, Cell이 클릭될 때 넘겨받게되고 , 그것을 기반으로 section0 , section1에 세팅해준다.

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/WeatherDetailVieModel.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/WeatherDetailVieModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class WeatherDetailViewModel {
+    private var sectionZeroViewModel = [LabelCellViewModel]()
+    private var sectionOneViewModel = LabelCellViewModel(title: "")
+    
+    func setupTopSectionViewModel(_ cellSummary : [String]) {
+        let cityName = cellSummary[0]
+        let currentTemperature = cellSummary[1]
+        let description = cellSummary[2]
+        
+        self.sectionZeroViewModel.append(LabelCellViewModel(title: cityName))
+        self.sectionZeroViewModel.append(LabelCellViewModel(title: currentTemperature))
+        self.sectionOneViewModel.title = description
+    }
+    
+    func fetchWeatherDetail(){
+        //GET요청
+    }
+    
+    func modelAt(_ indexPath : IndexPath) -> LabelCellViewModel?{
+        switch indexPath.section {
+        case 0:
+            return self.sectionZeroViewModel[indexPath.item]
+        case 1:
+            return self.sectionOneViewModel
+        case 2:
+            return nil
+        case 3:
+            return nil
+        default:
+            return nil
+        }
+    }
+}
+
