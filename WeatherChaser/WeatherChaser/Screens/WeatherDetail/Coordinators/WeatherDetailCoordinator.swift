5fc713554fbbaae7148b492faf6fc308a4c6225d
SONG <geun2121@gmail.com>
2023-12-23 04:56:26 +0900

    [FEAT] Cell 터치시 동작 구현
    - pullCellData() : cell을 터치하면 호출되고 , indexPath로 해당Cell의 viewModel에 접근하여 일부 정보를 넘겨받을 수 있다.
    - 그 이후, weatherDetailVC를 생성하고, setupViewModel로 초기화시켜준다.

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/Coordinators/WeatherDetailCoordinator.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/Coordinators/WeatherDetailCoordinator.swift
@@ -9,15 +9,27 @@ import UIKit
 
 class WeatherDetailCoordinator: Coordinator {
     private var presenter : UINavigationController
-    private var weatherDetailViewController : SettingsTableViewController?
+    //private var weatherDetailViewController : WeatherDetailViewController?
     
     init(presenter : UINavigationController){
         self.presenter = presenter
     }
     
     func changeViewController(with something : Any) {
+        guard let indexPath = something as? IndexPath else {
+            return
+        }
+        
+        guard let previousViewController = self.presenter.topViewController as? WeatherListTableViewController else {
+            return
+        }
+        
+        let cellSummary = previousViewController.pullCellData(at: indexPath)
+        
         let weatherDetailViewController = WeatherDetailViewController(nibName: nil, bundle: nil)
+        weatherDetailViewController.setupViewModel(cellSummary)
         weatherDetailViewController.title = "View in detail"
+        
         self.presenter.pushViewController(weatherDetailViewController, animated: true)
     }
     
