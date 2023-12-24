2a0a36f3dc4c38531d5d3f31b02ef773bf52fdd2
SONG <geun2121@gmail.com>
2023-12-25 02:34:37 +0900

    [ADD] 파일추가
    - 느슨한 결합을 위해 factory패턴 채용
    - 각 섹션에 삽입될 CellViewModel들은 프로토콜을 통과하여 WeatherDetailViewModel에서 사용된다.

---
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/Protocols/WeatherDetailViewModelFactory.swift
@@ -0,0 +1,12 @@
+//
+//  WeatherDetailViewModelBySection.swift
+//  WeatherChaser
+//
+//  Created by SONG on 2023/12/24.
+//
+
+import Foundation
+
+protocol WeatherDetailViewModelBySection {
+    func setViewModel(with something: Any?)
+}
