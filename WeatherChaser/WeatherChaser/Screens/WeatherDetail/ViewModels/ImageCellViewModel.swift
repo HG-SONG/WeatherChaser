7710d882535c36f43d18c1c98147b48400346b31
SONG <geun2121@gmail.com>
2023-12-26 18:55:42 +0900

    [FEAT] ImageCellViewModel 정의
    - ImageCellViewModel을 정의하였다.

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/ImageCellViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/ImageCellViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class ImageCellViewModel: WeatherDetailViewModelBySection {
+    private var title : String = ""
+    private var imageName : String = ""
+    private var texts = [String]()
+    
+    func setViewModel(with something: Any?) {
+    }
+    
+    func getViewModel() -> [String] {
+        var array = [title,imageName]
+        
+        for item in texts {
+            array.append(item)
+        }
+        return array
+    }
+    
+}
+
