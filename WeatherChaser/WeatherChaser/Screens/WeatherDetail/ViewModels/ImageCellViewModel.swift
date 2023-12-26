5a25ce5abb26ac21f272b69999e2dd38b4b3a450
SONG <geun2121@gmail.com>
2023-12-26 18:56:40 +0900

    [FEAT] setViewModel() 정의
    - 파라미터로 들어온 객체의 타입에 따라 조건분기후 , 프로퍼티 세팅 진행

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/ImageCellViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/ImageCellViewModel.swift
@@ -13,6 +13,27 @@ class ImageCellViewModel: WeatherDetailViewModelBySection {
     private var texts = [String]()
     
     func setViewModel(with something: Any?) {
+        if let data = something as? Clouds {
+            self.texts.append(data.all.convertToCloudCover())
+            self.imageName = "cloud.jpg"
+            self.title = "Cloud cover"
+        } else if let data = something as? Wind {
+            self.texts.append(data.deg.convertToDirection())
+            self.texts.append(data.speed.formatAsAmount() + "m/sec")
+            self.imageName = "wind.jpg"
+            self.title = "Wind"
+        } else if let data = something as? SunSetAndRise {
+            self.texts.append(data.sunrise.convertToHHmm(isSunrise: true))
+            self.texts.append(data.sunset.convertToHHmm(isSunrise: false))
+            self.imageName = "sunset.jpg"
+            self.title = "Sunrise & Sunset"
+        } else if let data = something as? Rain {
+            self.texts.append(data.amount.formatAsAmount() + "mm")
+            self.imageName = "precipitation.png"
+            self.title = "Precipitation 1h"
+        } else {
+            //Error
+        }
     }
     
     func getViewModel() -> [String] {
