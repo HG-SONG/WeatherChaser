16f8b899e59225b9baea19db7bf27c30d0caaec5
SONG <geun2121@gmail.com>
2023-12-26 18:53:07 +0900

    [FEAT] IntExtension 정의
    - int타입의 값들을 원하는 형태의 String으로 리턴하는 메소드 정의

--- WeatherChaser/WeatherChaser/Extensions/IntExtension.swift
+++ WeatherChaser/WeatherChaser/Extensions/IntExtension.swift
@@ -8,5 +8,30 @@
 import Foundation
 
 extension Int {
+    func convertToCloudCover() -> String {
+        return "\(self) / 100"
+    }
     
+    func convertToDirection() -> String {
+        let directions = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+        let index = Int((Double(self) / 45.0).rounded()) % 8
+        let direction = directions[index]
+        
+        return direction
+    }
+    
+    func convertToHHmm(isSunrise: Bool) -> String {
+        let timestamp = TimeInterval(self)
+        let date = Date(timeIntervalSince1970: timestamp)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        let formattedDate = dateFormatter.string(from: date)
+        
+        if isSunrise {
+            return "☀︎ " + formattedDate
+        } else {
+            return "☼ " + formattedDate
+        }
+    }
 }
