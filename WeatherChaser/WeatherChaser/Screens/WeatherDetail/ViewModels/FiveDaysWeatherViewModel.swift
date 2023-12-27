e831ff16617de22161bcd968674eb0a499acff1e
SONG <geun2121@gmail.com>
2023-12-28 01:48:05 +0900

    [FEAT] FiveDaysCellViewModel 정의
    - 프로토콜 명세 구현
    - 프로퍼티 정의

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/FiveDaysWeatherViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/FiveDaysWeatherViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class FiveDaysCellViewModel : WeatherDetailViewModelBySection {
+    private var list = [WeatherBy3hViewModel]()
+
+    func setViewModel(with something: Any?) {
+        guard let fiveDaysInfo = something as? WeatherDetailResponse else {
+            return
+        }
+        
+        for item in fiveDaysInfo.list {
+            let weatherBy3h = WeatherBy3hViewModel()
+            weatherBy3h.setupViewModel(item)
+            self.list.append(weatherBy3h)
+        }
+    }
+
+    func getViewModelAsObject() -> [Any] {
+        return [self.list]
+    }
+}
