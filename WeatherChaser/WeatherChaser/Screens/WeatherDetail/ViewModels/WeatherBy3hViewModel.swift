4fd5ad6ffbe01e06b79695b5f7f0e2a916c436e7
SONG <geun2121@gmail.com>
2023-12-28 01:24:08 +0900

    [FEAT] viewModel 정의
    - WeatherBy3hViewModel 의 내용을 정의했다.

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/WeatherBy3hViewModel.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/ViewModels/WeatherBy3hViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class WeatherBy3hViewModel {
+    private var date : String = ""
+    private var time : String = ""
+    private var imageName : String = ""
+    private var temparature : String = ""
+    private var rainPrecipitation : String = "⛆ "
+    private var snowPrecipitation : String = "❆ "
+    private var pop : String = ""
+    
+    func setupViewModel(_ viewModel : WeatherDetail) {
+        setupDateAndTime(fullDate: viewModel.dt)
+        self.imageName = viewModel.weather.first!.main
+        self.temparature = viewModel.main.temp.formatAsDegree()
+        self.rainPrecipitation += (viewModel.rain?.amountFor3h?.formatAsTwoDecimalPlaces() ?? "0.00mm")
+        self.snowPrecipitation += (viewModel.snow?.amountFor3h?.formatAsTwoDecimalPlaces() ?? "0.00mm")
+        self.pop = viewModel.pop.formatAsTwoDecimalPlaces()
+    }
+    
+    private func setupDateAndTime(fullDate: Int) {
+        let splited = fullDate.splitDate()
+        self.date = splited[0]
+        self.time = splited[1]
+    }
+    
+}
