0107299292fa2662b8d226f7eed6f0454717f73d
SONG <geun2121@gmail.com>
2023-12-29 17:00:12 +0900

    [FEAT] WeatherBy3hView 정의
    - viewModel의 데이터를 적용시켜 StackView를 정의하였다.

--- WeatherChaser/WeatherChaser/Screens/WeatherDetail/Views/WeatherBy3hView.swift
+++ WeatherChaser/WeatherChaser/Screens/WeatherDetail/Views/WeatherBy3hView.swift
@@ -5,4 +5,34 @@
 //  Created by SONG on 2023/12/28.
 //
 
-import Foundation
+import UIKit
+
+class WeatherBy3hView : UIStackView {
+    
+    func commonInit(viewModel : WeatherBy3hViewModel) {
+        self.axis = .vertical
+        self.alignment = .center
+        self.spacing = 5.0
+        setLabel(viewModel.date)
+        setLabel(viewModel.time)
+        setImageView(viewModel.imageName)
+        setLabel(viewModel.temparature)
+        setLabel(viewModel.rainPrecipitation)
+        setLabel(viewModel.snowPrecipitation)
+        setLabel(viewModel.pop)
+    }
+    
+    private func setLabel(_ text: String) {
+        let label = UILabel(frame: .zero)
+        label.text = text
+        self.addArrangedSubview(label)
+    }
+
+    private func setImageView(_ text: String) {
+        
+        let image = UIImage(systemName: text)
+        let imageView = UIImageView(image: image)
+        self.addArrangedSubview(imageView)
+    }
+
+}
