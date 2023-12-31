1e65e843e9f599980a93b9c2a8f5b4a907aa9445
SONG <geun2121@gmail.com>
2023-12-31 18:32:49 +0900

    [CHORE] 공백라인 제거

--- WeatherChaser/WeatherChaser/Application/AppDelegate.swift
+++ WeatherChaser/WeatherChaser/Application/AppDelegate.swift
@@ -9,7 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         UINavigationBar.appearance().barTintColor = .black
@@ -44,6 +44,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             userDefaults.set("metric", forKey: "unit")
             userDefaults.set("metric", forKey: "selected")
         }
+
     }
 }
-
