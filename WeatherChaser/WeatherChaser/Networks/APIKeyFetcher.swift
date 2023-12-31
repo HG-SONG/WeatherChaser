5d834781a2fd2f46d5f47a0268258ec834862a81
SONG <geun2121@gmail.com>
2023-12-31 18:21:27 +0900

    [FEAT] APIKeyFetcher 구현
    - 기존 하드코딩되어있던 APIKey를 없애고, GET요청으로 APIKey를 받아오는 방식으로 변경
    - APIKey요청이 성공하면, 환경변수에 저장하여 사용한다.

--- WeatherChaser/WeatherChaser/Networks/APIKeyFetcher.swift
+++ WeatherChaser/WeatherChaser/Networks/APIKeyFetcher.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+class APIKeyFetcher {
+    
+    static func fetchAPIKey(completion: @escaping (Result<String,Error>) -> Void) {
+        let userDefaults = UserDefaults.standard
+        
+        if userDefaults.value(forKey: "environmentVariable") == nil {
+            userDefaults.set("apiKey", forKey: "environmentVariable")
+        }
+        
+        guard let url = URL(string: "https://us-central1-weatherchaserapikey.cloudfunctions.net/getAPIKey") else {
+            return
+        }
+        
+        let resource = Resource(url:url ) { data in
+            let apiKey = String(data: data, encoding: .utf8)
+            return apiKey
+        }
+        
+        NetworkManager.fetch(resource: resource) { (result) in
+            switch result {
+            case .success(let apiKey) :
+                guard let environmentVariable = UserDefaults.standard.string(forKey: "environmentVariable") else {
+                    return
+                }
+                setenv(environmentVariable, apiKey,1)
+                completion(.success(apiKey))
+            case .failure(let error) :
+                print(error)
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
