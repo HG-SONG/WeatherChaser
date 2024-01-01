ac1662b8ae2d4a2bcd4af61a7b5f412f165977ee
SONG <geun2121@gmail.com>
2024-01-01 20:53:25 +0900

    [FEAT] LoadingManager 메소드 구현
    - showLoadingIndicator() : loadingIndicator가 이미 존재 하는지 아닌지 체크 후, loadingIndicator 표시
    - hideLoadingIndicator() : loadingIndicator 숨기기

--- WeatherChaser/WeatherChaser/Utils/LoadingManager.swift
+++ WeatherChaser/WeatherChaser/Utils/LoadingManager.swift
@@ -5,4 +5,38 @@
 //  Created by SONG on 2024/01/01.
 //
 
-import Foundation
+import UIKit
+
+class LoadingManager {
+    static func showLoadingIndicator() {
+        DispatchQueue.main.async {
+            guard let window = UIApplication.shared.windows.last else {
+                return
+            }
+            
+            let loadingIndicator: UIActivityIndicatorView
+            if let existedView = window.subviews.first(
+                where: { $0 is UIActivityIndicatorView } ) as? UIActivityIndicatorView {
+                loadingIndicator = existedView
+            } else {
+                loadingIndicator = UIActivityIndicatorView(style: .large)
+                loadingIndicator.frame = window.frame
+                loadingIndicator.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+                loadingIndicator.color = .white
+                
+                window.addSubview(loadingIndicator)
+            }
+            loadingIndicator.startAnimating()
+        }
+    }
+    
+    static func hideLoadingIndicator() {
+        DispatchQueue.main.async {
+            guard let window = UIApplication.shared.windows.last else {
+                return
+            }
+            window.subviews.filter({ $0 is UIActivityIndicatorView })
+                .forEach { $0.removeFromSuperview() }
+        }
+    }
+}
