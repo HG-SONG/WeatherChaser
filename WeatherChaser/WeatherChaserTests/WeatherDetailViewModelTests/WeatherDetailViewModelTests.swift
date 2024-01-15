9bedc71eca2dd7295dc0ffab93904ba3b7a28119
SONG <geun2121@gmail.com>
2024-01-15 18:29:30 +0900

    [FEAT] unitTest 코드 작성
    - WeatherDetailViewModelTests 내부 메소드에 대한 테스트 코드 작성

--- WeatherChaser/WeatherChaserTests/WeatherDetailViewModelTests/WeatherDetailViewModelTests.swift
+++ WeatherChaser/WeatherChaserTests/WeatherDetailViewModelTests/WeatherDetailViewModelTests.swift
@@ -8,28 +8,166 @@
 import XCTest
 
 final class WeatherDetailViewModelTests: XCTestCase {
-
+    
+    private var weatherDetailViewModel : WeatherDetailViewModel!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        self.weatherDetailViewModel = WeatherDetailViewModel()
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.weatherDetailViewModel = nil
+        try super.tearDownWithError()
+    }
+    
+    func testCommonInit() {
+        self.weatherDetailViewModel.commonInit()
+        
+        XCTAssertNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 1)))
+        XCTAssertNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 3)))
+    }
+    
+    func testSetupUpperSectionViewModel_Rain_Snow_are_nil() {
+        let cellSummary = WeatherCellViewModel()
+        self.weatherDetailViewModel.setupUpperSectionViewModel(cellSummary)
+        
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 0)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 1, section: 0)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 1)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 1, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 2, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 3, section: 2)))
+        
+        XCTAssertNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 3)))
+    }
+    
+    func testSetupUpperSectionViewModel_Rain_is_nil() {
+        let cellSummary = WeatherCellViewModel()
+        cellSummary.weather.snow = Snow(amountFor1h: 1.0, amountFor3h: 3.0)
+        
+        self.weatherDetailViewModel.setupUpperSectionViewModel(cellSummary)
+        
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 0)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 1, section: 0)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 1)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 1, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 2, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 3, section: 2)))
+        
+        XCTAssertNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 3)))
+    }
+    
+    func testSetupUpperSectionViewModel_Snow_is_nil() {
+        let cellSummary = WeatherCellViewModel()
+        cellSummary.weather.rain = Rain(amountFor1h: 1.0, amountFor3h: 3.0)
+        
+        self.weatherDetailViewModel.setupUpperSectionViewModel(cellSummary)
+        
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 0)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 1, section: 0)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 1)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 1, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 2, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 3, section: 2)))
+        
+        XCTAssertNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 3)))
+    }
+    
+    func testSetupUpperSectionViewModel_Rain_Snow_Notnil() {
+        let cellSummary = WeatherCellViewModel()
+        cellSummary.weather.rain = Rain(amountFor1h: 1.0, amountFor3h: 3.0)
+        cellSummary.weather.snow = Snow(amountFor1h: 1.0, amountFor3h: 3.0)
+        
+        self.weatherDetailViewModel.setupUpperSectionViewModel(cellSummary)
+        
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 0)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 1, section: 0)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 1)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 1, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 2, section: 2)))
+        XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 3, section: 2)))
+        
+        XCTAssertNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 3)))
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testSetupLowerSectionWithFetching() {
+        let city = "city"
+        let expectation = self.expectation(description: "success")
+        
+        weatherDetailViewModel.setupLowerSectionWithFetching(of: city) {
+            expectation.fulfill()
+            XCTAssertNotNil(self.weatherDetailViewModel.modelAt(IndexPath(item: 0, section: 3)))
         }
+        waitForExpectations(timeout: 1, handler: nil)
     }
+}
 
+
+class WeatherCellViewModel {
+    let city = "city"
+    let temperature = "0.0"
+    let weather = WeatherResponse()
+}
+
+struct WeatherDetailResponse: Codable {
+}
+
+class WeatherResponse {
+    let weather = [WeatherDesc()]
+    let sys = Mock()
+    let clouds = Mock()
+    let wind = Mock()
+    var rain = Rain(amountFor1h: nil, amountFor3h: nil)
+    var snow = Snow(amountFor1h: nil, amountFor3h: nil)
+}
+
+class WeatherDesc {
+    let description = "description"
+}
+
+class Mock : Codable {
+    
+}
+
+class Rain: Codable {
+    let amountFor1h : Double?
+    let amountFor3h : Double?
+    
+    init(amountFor1h: Double?, amountFor3h: Double?) {
+        self.amountFor1h = amountFor1h
+        self.amountFor3h = amountFor3h
+    }
+}
+
+class Snow: Codable {
+    let amountFor1h : Double?
+    let amountFor3h : Double?
+    
+    init(amountFor1h: Double?, amountFor3h: Double?) {
+        self.amountFor1h = amountFor1h
+        self.amountFor3h = amountFor3h
+    }
+}
+
+class LabelCellViewModel : WeatherDetailViewModelBySection {
+    func setViewModel(with something: Any?) {
+    }
+}
+
+class ImageCellViewModel : WeatherDetailViewModelBySection {
+    func setViewModel(with something: Any?) {
+    }
+}
+
+class FiveDaysCellViewModel : WeatherDetailViewModelBySection {
+    func setViewModel(with something: Any?) {
+    }
+}
+
+class WeatherDetail{
 }
